0697f298d18990fb19fc509aff6dc004fab9f3e3
Ivan Sharovarov <sharovarov31@gmail.com>
2020-07-05 14:03:54 +0600

    creating a bucket

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "github.com/IvanSharovarov/cli-task-manager/cmd"
+import (
+	"fmt"
+	"github.com/IvanSharovarov/cli-task-manager/cmd"
+	"github.com/IvanSharovarov/cli-task-manager/db"
+	"github.com/mitchellh/go-homedir"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-	cmd.RootCmd.Execute()
+	home, err := homedir.Dir()
+	must(err)
+
+	dbPath := filepath.Join(home, "tasks.db")
+	err = db.Init(dbPath)
+	must(err)
+
+	must(cmd.RootCmd.Execute())
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
